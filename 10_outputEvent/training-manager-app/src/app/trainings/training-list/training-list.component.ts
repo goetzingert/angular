d543cb50f9887e26916eb4c854ebc116c3b9e0fe
thomas.goetzinger <mail@simplydevelop.de>
2021-06-11 14:39:07 +0200

    Excercise 10

---
+++ 10_outputEvent/training-manager-app/src/app/trainings/training-list/training-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Training } from '../training.model';
+
+@Component({
+  selector: 'training-list',
+  templateUrl: './training-list.component.html',
+  styleUrls: ['./training-list.component.css']
+})
+export class TrainingListComponent implements OnInit {
+
+  private selectedTraining : Training | undefined;
+  @Input() trainings : Training[] = [];
+  @Output() trainingSelected : EventEmitter<Training> = new EventEmitter<Training>();
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  listItemClicked(event: MouseEvent, training: Training){
+    this.selectedTraining = training;
+    this.trainingSelected.emit(this.selectedTraining);
+  }
+
+}
